7a2eb6a8769882b81c014c9eca8ff144a661799d
gabrielchpv <uliseschapov97@gmail.com>
2022-05-25 15:33:46 -0300

    started creating Thumbnail component

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,6 @@
-import { Movie } from "../typings"
-
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import { Movie } from '../typings'
+import Thumbnail from './Thumbnail'
 
 interface Props {
   title: string
@@ -7,7 +8,24 @@ interface Props {
 }
 
 function Row({ title, movies }: Props) {
-  return <div>Row</div>
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon className=":hover-scale-125 absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9  cursor-pointer transition group-hover:opacity-100" />
+        <div className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+      
+        </div>
+        <ChevronRightIcon className=":hover-scale-125 absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9  cursor-pointer transition group-hover:opacity-100" />
+      </div>
+    </div>
+  )
 }
 
 export default Row
---
+++ components/Thumbnail.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Movie } from '../typings'
+
+interface Props {
+    movie: Movie
+}
+
+function Thumbnail({movie}: Props) {
+  return (
+    <div>Thumbnail</div>
+  )
+}
+
+export default Thumbnail
