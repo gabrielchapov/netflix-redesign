261ce7cb918a2bf441e4b306b048c912bd2115b3
gabrielchpv <uliseschapov97@gmail.com>
2022-05-31 16:57:28 -0300

    finished modal component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,13 @@
-import { XIcon } from '@heroicons/react/solid'
+import {
+  ThumbUpIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+} from '@heroicons/react/outline'
+import { PlusIcon, XIcon } from '@heroicons/react/outline'
 import MuiModal from '@mui/material/Modal'
 import { useEffect, useState } from 'react'
+import { FaPlay } from 'react-icons/fa'
+import ReactPlayer from 'react-player/lazy'
 import { useRecoilState } from 'recoil'
 import { modalState, movieState } from '../atoms/modalAtom'
 import { Element, Genre } from '../typings'
@@ -10,6 +17,7 @@ function Modal() {
   const [movie, setMovie] = useRecoilState(movieState)
   const [trailer, setTrailer] = useState('')
   const [genres, setGenres] = useState<Genre[]>([])
+  const [muted, setMuted] = useState(true)
 
   useEffect(() => {
     if (!movie) return
@@ -40,11 +48,14 @@ function Modal() {
     setShowModal(false)
   }
 
-  console.log(trailer);
-  
+  console.log(trailer)
 
   return (
-    <MuiModal open={showModal} onClose={handleClose}>
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
       <>
         <button
           onClick={handleClose}
@@ -52,7 +63,68 @@ function Modal() {
         >
           <XIcon className="h-6 w-6" />
         </button>
-        <div></div>
+        <div className="relative pt-[56.25%]">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: 'absolute', top: '0', left: '0' }}
+            playing
+            muted={muted}
+          />
+
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-4">
+              <button className=" flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition duration-200 hover:scale-105 hover:bg-[#e6e6e6]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+              <button className="flex h-11 w-11 items-center justify-center rounded-full border-2 border-[gray] bg-[#2a2a2a]/60 transition hover:scale-105 hover:border-white hover:bg-white/10 ">
+                <PlusIcon className="h-7 w-7" />
+              </button>
+              <button className="flex h-11 w-11 items-center justify-center rounded-full border-2 border-[gray] bg-[#2a2a2a]/60 transition hover:scale-105 hover:border-white hover:bg-white/10">
+                <ThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+
+            <button
+              className="flex h-11 w-11 items-center justify-center rounded-full border-2 border-[gray] bg-[#2a2a2a]/60 transition hover:scale-105 hover:border-white hover:bg-white/10"
+              onClick={() => setMuted(!muted)}
+            >
+              {muted ? (
+                <VolumeOffIcon className="h-6 w-6" />
+              ) : (
+                <VolumeUpIcon className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie!.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+
+            <div>
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres.map((genre) => genre.name).join(', ')}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     </MuiModal>
   )
