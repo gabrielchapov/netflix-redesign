bd10449a024be362f23c040ea6b68e6ddf2fc432
gabrielchpv <uliseschapov97@gmail.com>
2022-06-02 11:38:46 -0300

    started plans component

---
+++ components/Plans.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import React from 'react'
+
+function Plans() {
+  return (
+    <div>
+        <Head>
+            <title>Netflix</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <header>
+            <img src=""/>
+        </header>
+    </div>
+  )
+}
+
+export default Plans
